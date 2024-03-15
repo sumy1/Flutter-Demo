b744a7ce09b999e193ac05c75564ebaca61d8b06
sumy1 <hafzal446@gmail.com>
2024-03-15 16:07:02 +0530

    Evening Comment
    This is just the starting flutter project for Criconet Demo -

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Ciconet Demo Home Page'),
+      home: const MyHomePage(title: 'Criconet Demo Home Page'),
     );
   }
 }
